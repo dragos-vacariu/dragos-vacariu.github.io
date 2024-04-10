aba1d044cdcecf234d27d872f55dbe7d6e24bd08
Dragos Vacariu <blacky_d2@mail.com>
2024-04-10 09:38:47 +0300

    updates
    updates

--- scripts/global_script.js
+++ scripts/global_script.js
@@ -1,7 +1,19 @@
 	var app = angular.module('myApplication', []);
 	app.controller('Controller', function($scope) {
 		//Initializing the models;
-		$scope.template_info = ["Author: Dragos Vacariu", "Design: 2017"]
-		$scope.tools_used = ["HTML4", "HTML5", "CSS3", "CSS"]
-		$scope.connections_list = ["tutorialpoint.com"]
+		$scope.pageObject = {
+			footer_table_headers : ["Template Info:", "Connections:", "Tools Used:"],				
+			template_info : ["Author: Dragos Vacariu", "Design: 2017"],
+			tools_used : ["HTML4", "HTML5", "CSS3", "CSS"],
+			connections_list : ["tutorialpoint.com"],
+			footer_copyright : "Dragos Vacariu © 2024",
+			footer_paragraph : "Portofolio",
+			page_title : "Portofolio",
+			page_navigation : [
+				{name: "Home", value : "./index.html"},
+				{name: "Catalogue", value : "./catalogue.html"},
+				{name: "Contact", value : "./contact.html"},
+			]
+		}
+
 	});
