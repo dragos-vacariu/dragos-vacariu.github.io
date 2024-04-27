e21c8bb1b86d74cd6af27644e6bfc4bbd7baac32
dragos.vacariu <dragos.vacariu@mail.com>
2024-04-28 00:51:35 +0300

    Update portal_script.js

--- website_resources/local/scripts/portal_script.js
+++ website_resources/local/scripts/portal_script.js
@@ -58,7 +58,6 @@ function setCookie()
 		alert("pairs: " + pairs[0] + " = " + pairs[1]);
 		if(pairs[0].includes("view"))
 		{
-			alert("Here");
 			if(pairs[1].includes("paragraph"))
 			{
 				//Set paragraph view to true
