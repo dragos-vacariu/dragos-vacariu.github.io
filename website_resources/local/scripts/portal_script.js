134fd67a780b2a2ecc32cfa0d9d03a39ced82d6f
dragos.vacariu <dragos.vacariu@mail.com>
2024-04-27 23:16:35 +0300

    Update portal_script.js

--- website_resources/local/scripts/portal_script.js
+++ website_resources/local/scripts/portal_script.js
@@ -42,7 +42,7 @@ window.onload  = function () {
 function cookieHandling()
 {
 	//reading from the cookie file:
-	console.log(document.cookie)
+	alert(document.cookie)
 	if(document.cookie.length > 0)
 	{
 		setCookie();
@@ -62,13 +62,13 @@ function setCookie()
 			{
 				//Set paragraph view to true
 				view_selection.children(1).value = true;
-				console.log("Setting paragraph view");
+				alert("Setting paragraph view");
 			}
 			else
 			{
 				//Set table view to true
 				view_selection.children(0).value = true;
-				console.log("Setting paragraph view");
+				alert("Setting paragraph view");
 			}
 		}
 	}
@@ -99,7 +99,7 @@ function updateCookie(property, value)
 		var element = String(property + "=" + value + ";");
 		document.cookie += element;
 	}
-	console.log(document.cookie);
+	alert(document.cookie);
 }
 
 class Programming_Language
